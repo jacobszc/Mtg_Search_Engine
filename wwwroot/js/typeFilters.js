1dec6904b5d6144dbb32bf5daecd92bed43ea0ab
Jacob Szczudlik <jacobszc@buffalo.edu>
2025-12-11 10:29:36 -0500

    added type filter javascript

--- wwwroot/js/typeFilters.js
+++ wwwroot/js/typeFilters.js
@@ -1,1 +1,54 @@
-const 
+
+function filterTypes() {
+
+    const Artifact = document.getElementById("artifact")
+    const Instant = document.getElementById("instant")
+    const Sorcery = document.getElementById("sorcery")
+    const Enchantment = document.getElementbyId("enchantment")
+    const Land = document.getElementById("land")
+    const Planeswalker = document.getElementById("planeswalker")
+    const Battle = document.getElementById("battle")
+
+
+
+
+let typeFilter =[];
+
+if(Artifact.selected) {
+
+    typeFilter.push("Artifact")
+}
+
+if(Instant.selected) {
+
+    typeFilter.push("Instant")
+}
+
+if(Sorcery.selected) {
+
+    typeFilter.push("Sorcery")
+}
+
+if(Enchantment.selected) {
+
+    typeFilter.push("Enchantment")
+}
+
+if(Land.selected) {
+
+    typeFilter.push("Land")
+}
+
+if(Planeswalker.selected) {
+
+    typeFilter.push("Planeswalker")
+}
+
+if(Battle.selected) {
+
+    typeFilter.push("Battle")
+}
+
+return typeFilter
+
+}
