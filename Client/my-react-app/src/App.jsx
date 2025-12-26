2a6ddb0c51a612ead68561a49e7ebe5a2f785f1f
Jacob Szczudlik <jacobszc@buffalo.edu>
2025-12-26 13:03:51 -0500

    got ceentered content and search bar div refactored

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import SearchComp from './SearchComp'
 import './App.css'
 
 function App() {
   
 
   return  (
-
+   
     <div class = "centered-content">
 
+    
+     <SearchComp/>
 
 
 
-    </div> // end centered content div
+    </div>  
+
+    
   )
   
 }
---
+++ Client/my-react-app/src/SearchComp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function SearchComp() {
+
+
+    return (
+
+    <div class="searchbar-row">
+    <input id="input-bar" type="text" placeholder="Enter card name (e.g., Sakura Tribe Elder)" />
+    <button id="search-button">Search</button>
+   </div>
+
+
+
+    )
+
+
+    }
+
+
+export default SearchComp;
