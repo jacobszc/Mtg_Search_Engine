97bdb40ec0a00b748ad9a25f2ef94bc29a767805
Jacob Szczudlik <jacobszc@buffalo.edu>
2026-01-02 09:33:08 -0500

    set up fetch for card images

--- Client/my-react-app/src/SearchComp.jsx
+++ Client/my-react-app/src/SearchComp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TestImg from "/assets/testcard.png";
 import ResultComp from "./ResultsComp";
 
@@ -6,9 +6,39 @@ import ResultComp from "./ResultsComp";
 
 
 
-function SearchComp({setResultImgList}) {
+
+function SearchComp() {
+
   
-    const[input, setInput] = useState("");
+ 
+
+ const [urltoImg, seturlToImg] = useState("")
+ const [input, setInput] = useState("");  /// dynamcily keep track of user inpuit
+ const [dataIsLoaded, setdataIsLoaded ] = useState(false); // stsae machine to update if the data was fecthed proplety
+ 
+ 
+ 
+  
+   useEffect( () =>{
+    fetch(urltoImg)
+    .then((response)=> response.text()) // response is an unroslved promise to some text
+    .then((text) => {
+       seturlToImg(text);
+      setdataIsLoaded(true);
+     
+
+    });
+
+   
+  
+  }, [urltoImg]); // pass url as a depeendecy. use effect will get called every time url changes
+  
+  if(!dataIsLoaded) {
+    return (
+
+      <div> <h1>Please wait some time....</h1></div>
+
+     )} // end if check for loading data
     
 
     
@@ -29,7 +59,7 @@ function SearchComp({setResultImgList}) {
         onChange = {(e)=> setInput(e.target.value)}
     />
 
-  <button id="search-button" onClick = {()=> setResultImgList(prev => [...prev, input])}> 
+  <button id="search-button" onClick = {()=> seturlToImg(`/api/imagesearch/imagesearch?imgQueryResult=${encodeURIComponent({input})}`)}> 
     {/* need () => otherwise react wil call setResultImgList immediatly and assin the return to the button  */}
         click 
     </button>
--- obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cdb5c949348c2b5a0ee340d3085853a8a8566b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51eae5c6baa3c0108835a7e1c006cec476f8ad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
